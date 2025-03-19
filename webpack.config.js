8b34b7281d0e8f1e5bca8b0e49b0eed316406063
zonym <202871893+zonym@users.noreply.github.com>
2025-03-19 21:05:26 +0800

    feat: `webpack` optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const { resolve } = require('node:path');
 
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const isDev = process.env['NODE_ENV'] === 'development';
+
+/** @type {import('webpack').Configuration} */
 module.exports = {
-    mode: 'development',
+    mode: isDev ? 'development' : 'production',
     entry: resolve(__dirname, 'src/index.ts'),
     output: {
         path: resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: `scripts/${isDev ? '[name]' : '[chunkhash]'}.js`,
+        clean: !isDev
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
@@ -15,16 +20,25 @@ module.exports = {
     module: {
         rules: [
             {
+                include: resolve(__dirname, 'src'),
                 test: /\.[jt]sx?$/,
                 loader: 'babel-loader'
             },
             {
                 test: /.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    isDev ? 'style-loader' : MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
             }
         ]
     },
     plugins: [
+        isDev
+            ? void 0
+            : new MiniCSSExtractPlugin({
+                  filename: 'styles/[chunkhash].css'
+              }),
         new HTMLWebpackPlugin({
             template: resolve(__dirname, 'public/index.html')
         })
@@ -34,5 +48,33 @@ module.exports = {
         static: {
             directory: resolve(__dirname, 'static')
         }
-    }
+    },
+    optimization: isDev
+        ? {}
+        : {
+              splitChunks: {
+                  cacheGroups: {
+                      defaultVendors: false,
+                      defaults: {
+                          reuseExistingChunk: true,
+                          minChunks: 2,
+                          priority: -20
+                      },
+                      common: {
+                          test: /[/\\]node_modules[/\\]/,
+                          priority: -5,
+                          reuseExistingChunk: true,
+                          chunks: 'initial',
+                          name: 'common',
+                          minSize: 0
+                      },
+                      react: {
+                          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+                          name: 'vendor@react',
+                          chunks: 'all',
+                          priority: 10
+                      }
+                  }
+              }
+          }
 };
